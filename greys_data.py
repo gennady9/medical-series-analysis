463a2851964ff737440c5fb1d78b227435b8f200
gennady9 <gennady9@gmail.com>
2020-02-06 12:59:18 +0200

    greys anatomy reading pattients

--- greys_data.py
+++ greys_data.py
@@ -6,6 +6,8 @@ import requests
 
 def get_episode_data(page):
     episode_data = {}
+    patient_data = {}
+
     soup = BeautifulSoup(page.content, 'html.parser')
     data_div = soup.find('div', attrs={'id': "mw-content-text"})
     # = data_div.find_next("h2")
@@ -13,19 +15,49 @@ def get_episode_data(page):
     while h2_headline is not None:
         if h2_headline.find('span', attrs={'id': "Medical_Notes"}):
             break
-        h2_headline = h2_headline.find_next("h2")
+        else:
+            h2_headline = h2_headline.find_next("h2")
+    #print(h2_headline.parent)
+    diagnose_h = soup.find(text="Diagnosis:").parent.parent
+   # patient_diseases = diagnose_h.find_all("li")
+    patient_diagnose = []
+    for li in diagnose_h.findAll('li'):
+        patient_diagnose.append(li.text.strip())
+    #print(patient_diagnose)
+    patient_h3 = diagnose_h.parent.previous_sibling.previous_sibling
+    patient_link = patient_h3.find('a')
+    patient_name = patient_link.attrs['title']
+    if patient_link.has_attr('href'):
+        patient_url = patient_link.attrs['href']
+    print(patient_name)
+    print(patient_url)
+    #print(type(soup.find(text="Diagnosis:").parent.parent))
 
-    
-    temp_patient_data = {}
-    while h2_headline.next():
-        # patient headline
-        temp_patient = h2_headline.next()
-        temp_patient_data['name'] = temp_patient.find('mw-headline')
-        temp_patient_link = temp_patient.find('a')
-        if temp_patient_link is not None:
-            if temp_patient_link.has_attr('href'):
-                temp_patient_data['patient_link'] = temp_patient.find('a')
+    #patient_diseases = [li.text.strip() for ul in diagnose_h for li in diagnose_h.findAll('li')]
+   # print(patient_diseases)
+    #print(h2_headline.next_sibling.next_element)
+    #h3head = h2_headline.next_sibling
+    #patient_data['name'] = \
+    #print(type(h3head))
 
+    #print(h2_headline.find_next().find_next().find_next().find_next().find_next())
+    '''
+    temp = h2_headline.find_next()
+    print(h2_headline.name)
+    print(h2_headline.find_next().next())
+    while h2_headline.next().name != 'h2':
+        if h2_headline.name == 'h3':
+            temp_patient_data = {}
+            temp_patient = h2_headline.next()
+            temp_patient_data['name'] = temp_patient.find('mw-headline')
+            temp_patient_link = temp_patient.find('a')
+            if temp_patient_link is not None:
+                if temp_patient_link.has_attr('href'):
+                    temp_patient_data['patient_link'] = temp_patient.find('a')
+        if h2_headline.name == 'ul':
+            h2_field = h2_headline.find('b')
+            print("whatever")
+'''
 
 
 
